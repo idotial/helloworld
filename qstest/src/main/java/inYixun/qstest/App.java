406a89d708a4b358adc8012a11d0d9c2e7e139d2
idotial <724104612@qq.com>
2016-07-20 22:23:08 +0800

    web project

---
+++ qstest/src/main/java/inYixun/qstest/App.java
@@ -0,0 +1,13 @@
+package inYixun.qstest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ qstest/src/main/java/inYixun/qstest/test.java
@@ -0,0 +1,10 @@
+package inYixun.qstest;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("fjfsoj");
+	}
+
+}
